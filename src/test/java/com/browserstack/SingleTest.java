e10046b206f24e8d241ea7557477387f1290e659
sandipjos <sandeep.jose@cognizant.com>
2017-02-19 05:51:25 +0530

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -10,10 +10,11 @@ public class SingleTest extends BrowserStackJUnitTest {
 
   @Test
   public void test() throws Exception {
-    driver.get("https://www.google.com/");
-    driver.findElement(By.cssSelector("#lst-ib")).click();
-    driver.findElement(By.cssSelector("#lst-ib")).clear();
-    driver.findElement(By.cssSelector("#lst-ib")).sendKeys("massmutual");
+    driver.get(baseUrl + "about:home");
+    driver.findElement(By.id("searchText")).clear();
+    driver.findElement(By.id("searchText")).sendKeys("test");
+    driver.findElement(By.id("searchSubmit")).click();
+    driver.findElement(By.id("yui_3_10_0_1_1487463576596_198")).click();
     //driver.findElement(By.xpath("//div/div[3]/form/div[2]/div[2]/div[1]/div[1]/div[3]/div/div/div[3]/div/input[1]")).click();
     //driver.findElement(By.xpath("//div/div[3]/form/div[2]/div[2]/div[1]/div[1]/div[3]/div/div/div[3]/div/input[1]")).sendKeys("\n");
     Thread.sleep(5000);
